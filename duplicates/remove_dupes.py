e07134a75cb9b7769ef64e153b838c049d961317
Mike Turley <mike@miketurley.com>
2014-03-11 00:22:39 -0400

    implemented remove dupes (untested)

--- duplicates/remove_dupes.py
+++ duplicates/remove_dupes.py
@@ -1,2 +1,18 @@
 #!/usr/bin/env python
-# TODO implement the remove function
+
+import sys
+
+def main():
+  if len(sys.argv) < 3:
+    print 'Usage: '+sys.argv[0]+' <input-file> <output-file>'
+  input_file = open(sys.argv[1], 'r')
+  output_file = open(sys.argv[2], 'w')
+  last_id = ''
+  for line in input_file:
+    id_field = line.split(',')[0]
+    if id_field != last_id:
+      output_file.write(line)
+    last_id = id_field
+
+if __name__ == '__main__':
+  main()
