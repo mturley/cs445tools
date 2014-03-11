36ba6d181bb1aa261c5a9493594b4d066c02db1e
Mike Turley <mike@miketurley.com>
2014-03-11 00:33:07 -0400

    logging output and saving filtered rows

--- duplicates/remove_dupes.py
+++ duplicates/remove_dupes.py
@@ -8,11 +8,19 @@ def main():
   input_file = open(sys.argv[1], 'r')
   output_file = open(sys.argv[2], 'w')
   last_id = ''
+  total_count = 0
+  dupe_count = 0
   for line in input_file:
     id_field = line.split(',')[0]
     if id_field != last_id:
       output_file.write(line)
+    else:
+      dupe_count += 1
+      print 'REMOVED DUPLICATE: "'+line+'"'
     last_id = id_field
+    total_count += 1
+  print 'Complete! Removed '+str(dupe_count)+' duplicate-ID rows out of '+str(total_count)+' total rows.'
+  print str(total_count - dupe_count) +' lines were copied to '+sys.argv[2]+'.'
 
 if __name__ == '__main__':
   main()
